26fe51aa309bfa23bf1899c55654256da8ae70cc
Maxh <44454544+1-max-1@users.noreply.github.com>
2023-08-06 21:09:54 +1200

    Trying out the screen size change event callback. It's kind of janky and not working too well. Keeping in another branch for future reference.

---
+++ ArcadyMenu/wwwroot/screenSizeObserver.js
@@ -0,0 +1,17 @@
+﻿var dotNetHelper;
+var screenIsCurrentlyLarge;
+
+screenIsLarge = () => document.querySelector("html").clientWidth >= 768;
+
+window.initializeScreenSizeObserver = objRef => {
+	dotNetHelper = objRef;
+	screenIsCurrentlyLarge = screenIsLarge();
+	return screenIsLarge();
+};
+
+window.addEventListener("resize", () => {
+	if (screenIsCurrentlyLarge != screenIsLarge()) {
+		screenIsCurrentlyLarge = screenIsLarge();
+		dotNetHelper.invokeMethodAsync("OnScreenSizeChanged", screenIsCurrentlyLarge);
+	}
+});
